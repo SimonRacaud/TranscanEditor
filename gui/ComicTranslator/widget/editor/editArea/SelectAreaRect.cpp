de95c7c9d784cc62e7f508e556c88f9d353329c6
srac0005 <srac0005@student.monash.edu>
2022-09-10 18:37:50 +1000

    feat(gui/widget): select area rect. select or not a rect

---
+++ gui/ComicTranslator/widget/editor/editArea/SelectAreaRect.cpp
@@ -0,0 +1,45 @@
+#include "SelectAreaRect.h"
+
+#include <QPainter>
+
+SelectAreaRect::SelectAreaRect(BlockCluster const &cluster)
+    : _data(cluster)
+{
+
+}
+
+/** Public **/
+
+void SelectAreaRect::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
+{
+    QRectF rect = this->boundingRect();
+    QColor borderColor = (_selected) ? Qt::black : Qt::red;
+    QPen pen(borderColor, 2 /* Line width */, Qt::DashDotLine);
+
+    painter->setPen(pen);
+    if (_selected) {
+        painter->setBrush(QColor(255, 255, 255, 125)); // background color
+    } else {
+        painter->setBrush(Qt::transparent); // background color
+    }
+    painter->drawRect(rect);
+}
+
+QRectF SelectAreaRect::boundingRect() const
+{
+    return QRectF(_data.box.x(), _data.box.y(),
+                  _data.box.width(), _data.box.height());
+}
+
+bool SelectAreaRect::isSelected() const
+{
+    return _selected;
+}
+
+/** Protected **/
+
+void SelectAreaRect::mousePressEvent(QGraphicsSceneMouseEvent *event)
+{
+    _selected = !_selected;
+    update();
+}
---
+++ gui/ComicTranslator/widget/editor/editArea/SelectAreaRect.h
@@ -0,0 +1,32 @@
+#ifndef SELECTAREARECT_H
+#define SELECTAREARECT_H
+
+#include <QGraphicsObject>
+#include <QObject>
+
+#include "include/model/OCRPage.h"
+
+class SelectAreaRect : public QGraphicsObject
+{
+    Q_OBJECT
+public:
+    SelectAreaRect(BlockCluster const &data);
+
+    virtual void paint(
+            QPainter *painter,
+            const QStyleOptionGraphicsItem *option,
+            QWidget *widget) override;
+
+    QRectF boundingRect() const override;
+
+    bool isSelected() const;
+
+protected:
+    virtual void mousePressEvent(QGraphicsSceneMouseEvent *event) override;
+
+private:
+    BlockCluster _data;
+    bool _selected{false};
+};
+
+#endif // SELECTAREARECT_H
